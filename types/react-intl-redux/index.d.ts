2938ac2da65f74941f071d1440512bfa092c7690
James Wright <james@jamesswright.co.uk>
2018-07-16 14:05:56 +0100

    Supports intlSelector property for react-intl-redux

--- types/react-intl-redux/index.d.ts
+++ types/react-intl-redux/index.d.ts
@@ -1,10 +1,11 @@
-// Type definitions for react-intl-redux v0.1.0
+// Type definitions for react-intl-redux
 // Project: https://github.com/ratson/react-intl-redux
 // Definitions by: Karol Janyst <https://github.com/LKay>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.8
+// TypeScript Version: 2.6
 
-import { Action, AnyAction } from "redux"
+import { Component } from "react";
+import { Action } from "redux"
 import { Provider as ReduxProvider } from "react-redux"
 import { IntlProvider as ReactIntlProvider } from "react-intl"
 
@@ -13,11 +14,15 @@ interface IntlState {
     messages: any
 }
 
+interface IntlProviderProps {
+  intlSelector?: ({ intl }: { intl: IntlState }) => IntlState;
+}
+
 interface IntlAction extends Action {
     payload?: IntlState
 }
 
 export function intlReducer(state: IntlState, action: IntlAction): IntlState
 export function updateIntl (opts: IntlState): IntlAction
-export class IntlProvider extends ReactIntlProvider {}
-export class Provider<A extends Action = AnyAction> extends ReduxProvider<A> {}
+export class IntlProvider extends Component<ReactIntlProvider.Props & IntlProviderProps> {}
+export class Provider extends ReduxProvider {}
